6b5ad156b2700ab93a389241e7ce286ee064a2f9
Walmyr <wlsf82@gmail.com>
2026-01-02 00:20:04 +0100

    Rename the app

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['pwa.svg'],
       manifest: {
-        name: 'Building Emoji',
-        short_name: 'BuildingEmoji',
-        description: 'Click emojis in windows; avoid the bomb.',
+        name: 'Hotel Emojis',
+        short_name: 'Hotel Emojis',
+        description: 'Collect emojis to win (or lose) points; avoid bombs.',
         theme_color: '#111827',
         background_color: '#111827',
         display: 'standalone',
